2cc8e14bbe0d1a86cc66e240e9c0e60d56f86306
Sarath <cheekatisarath@gmail.com>
2023-11-09 20:24:28 +0530

    till tailwind css lecture

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,21 +1,25 @@
-import Header from "./Header";
-import CartOverView from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "./Loader";
+import Header from './Header';
+import Loader from './Loader';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
-      <main>
-        <h1>Content</h1>
-        <Outlet />
-      </main>
-      <CartOverView />
+
       <Header />
+
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+
+      <CartOverview />
     </div>
   );
 }
