97e247bead46df6286d2591c91f8f2f45e4cf5e3
Sarath <cheekatisarath@gmail.com>
2023-11-10 09:41:22 +0530

    till 314

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,18 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Jonas</div>;
+  const { username } = useSelector((state) => state.user);
+
+  if (!username)
+    return (
+      <div className="hidden text-sm font-semibold md:block">
+        Hello user !!!
+      </div>
+    );
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
---
+++ src/store.js
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './features/user/userSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'username',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
+const store = configureStore({
+  reducer: {
+    user: persistedReducer,
+  },
+});
+
+export const persistor = persistStore(store);
+
+export default store;
